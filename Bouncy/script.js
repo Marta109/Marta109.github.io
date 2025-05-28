88d1fa1773c48cc698ecfc088db4b7fbeaf356cd
Marta <71411783+Marta109@users.noreply.github.com>
2025-05-29 00:05:27 +0300

    Delete Bouncy directory

--- Bouncy/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-  var navbar = document.querySelector("header");
-  if (window.scrollY > 0) {
-    navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  } else {
-    navbar.style.background = "transparent";
-  }
-});
